fb09da0099db6ff2860ca08c3679670632425ce1
Arthur <arthurd800@wp.pl>
2020-03-03 13:58:00 +0100

    added Navbar with MUI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.sass';
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Our App</h1>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+          </Switch>
+        </div>
+      </Router>  
     </div>
   );
 }
