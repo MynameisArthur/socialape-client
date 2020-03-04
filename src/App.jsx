a182c953c74df76ffd3b7f46a386e057f11c2754
Arthur <arthurd800@wp.pl>
2020-03-04 11:10:55 +0100

    added Scream component, Home component fetches screams from firebase using axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,42 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Navbar from './components/Navbar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bdc4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondray: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/signup" component={Signup}/>
-          </Switch>
-        </div>
-      </Router>  
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/signup" component={Signup}/>
+            </Switch>
+          </div>
+        </Router>  
+      </div>
+    </MuiThemeProvider>
   );
 }
 
