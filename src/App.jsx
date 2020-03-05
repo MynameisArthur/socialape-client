c4f43f8178183349424f47a89017faff427d50b4
Arthur <arthurd800@wp.pl>
2020-03-05 12:16:29 +0100

    Added Signup component

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,28 @@ import Signup from './pages/signup';
 import Navbar from './components/Navbar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
+import AuthRoute from './util/AuthRoute';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bdc4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
+
+const theme = createMuiTheme(themeFile);
+
+const token = localStorage.FBIdToken;
+let authenticated; 
+if(token)
+{
+  const decodedToken = jwtDecode(token);  
+  if(decodedToken.exp * 1000 < Date.now())
+  {
+    window.location.href = '/login';
+    authenticated = false;
   }
-});
+  else{
+    authenticated = false;
+  }
+}
+
 
 function App() {
   return (
@@ -34,8 +39,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/signup" component={Signup}/>
+              <AuthRoute path="/login" component={Login} authenticated={authenticated}/>
+              <AuthRoute path="/signup" component={Signup} authenticated={authenticated}/>
             </Switch>
           </div>
         </Router>  
